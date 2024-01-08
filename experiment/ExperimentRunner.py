0d6312ea5baca997af7e09712953a610c20bb6a3
Katasc22 <k.aschauer@student.tugraz.at>
2024-01-08 13:27:07 +0100

    adaptations to include distil into framework

--- experiment/ExperimentRunner.py
+++ experiment/ExperimentRunner.py
@@ -2,34 +2,41 @@ from .data.PreprocessedData import PreprocessedData
 from .data.ExperimentData import ExperimentResultData
 from .approaches.translation_based import retrieve_k_documents_per_query_tb_monolingual
 from .approaches.multilingual_bert import retrieve_k_documents_per_query_mbert
+from .approaches.knowledge_distillation import retrieve_k_documents_per_query_distiluse
+
 
 class ExperimentRunner:
-	def __init__(self, experiment_approach: str, experiment_mode: str, preprocessed_data: PreprocessedData, translationHandler, device):
-		self.experiment_approach = experiment_approach
-		self.experiment_mode = experiment_mode
-		self.preprocessed_data = preprocessed_data
-		self.translationHandler = translationHandler
-		self.device = device
+    def __init__(self, experiment_approach: str, experiment_mode: str, preprocessed_data: PreprocessedData,
+                 translationHandler, device):
+        self.experiment_approach = experiment_approach
+        self.experiment_mode = experiment_mode
+        self.preprocessed_data = preprocessed_data
+        self.translationHandler = translationHandler
+        self.device = device
 
+    def runExperiment(self):
+        print("[ExperimentRunner] Start experiment ...")
+        if self.experiment_approach == "all":
+            print(self.device)
+        elif self.experiment_approach == "translation-based":
+            if self.experiment_mode == "monolingual":
+                retrieved_docs_per_query = retrieve_k_documents_per_query_tb_monolingual(self.preprocessed_data.queries,
+                                                                                         self.preprocessed_data.docs,
+                                                                                         15, device=self.device)
 
-	def runExperiment(self):
-		print("[ExperimentRunner] Start experiment ...")
-		if self.experiment_approach == "all":
-			print(self.device)
-		elif self.experiment_approach == "translation-based":
-			if self.experiment_mode == "monolingual":
-				retrieved_docs_per_query = retrieve_k_documents_per_query_tb_monolingual(self.preprocessed_data.queries, self.preprocessed_data.docs, 
-																				15, device=self.device)
-			
-				return (ExperimentResultData("translation-based", retrieved_docs_per_query, 15), )
-			
-			elif self.experiment_mode == "multilingual":
-				pass
-		
-		elif self.experiment_approach == "ml-mbert":
-			retrieved_docs_per_query  = retrieve_k_documents_per_query_mbert(self.preprocessed_data.queries, self.preprocessed_data.docs, 
-																			15, device=self.device)
-			return (ExperimentResultData("ml-mbert", retrieved_docs_per_query, 15), )
-		
-		elif self.experiment_approach == "ml_knowledge_distillation":
-			pass
+                return (ExperimentResultData("translation-based", retrieved_docs_per_query, 15),)
+
+            elif self.experiment_mode == "multilingual":
+                pass
+
+        elif self.experiment_approach == "ml-mbert":
+            retrieved_docs_per_query = retrieve_k_documents_per_query_mbert(self.preprocessed_data.queries,
+                                                                            self.preprocessed_data.docs,
+                                                                            15, device=self.device)
+            return (ExperimentResultData("ml-mbert", retrieved_docs_per_query, 15),)
+
+        elif self.experiment_approach == "ml_knowledge_distillation":
+            retrieved_docs_per_query = retrieve_k_documents_per_query_distiluse(self.preprocessed_data.queries,
+                                                                                self.preprocessed_data.docs,
+                                                                                15, device=self.device)
+            return (ExperimentResultData("ml_knowledge_distillation", retrieved_docs_per_query, 15),)
